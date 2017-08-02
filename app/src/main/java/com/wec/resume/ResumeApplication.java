18ca549fcd5a5117166600daf189ba20c2e0c502
Majfrendmartin <raciborski.p@gmail.com>
2017-08-02 13:26:56 +0200

    Removed wrong headers

--- app/src/main/java/com/wec/resume/ResumeApplication.java
+++ app/src/main/java/com/wec/resume/ResumeApplication.java
@@ -6,17 +6,6 @@ import com.wec.resume.injection.component.ApplicationComponent;
 import com.wec.resume.injection.component.DaggerApplicationComponent;
 import com.wec.resume.injection.module.ApplicationModule;
 
-/*
- * Avaya Inc. - Proprietary (Restricted)
- * Solely for authorized persons having a need to know
- * pursuant to Company instructions.
- *
- * Copyright 2017. Avaya Inc. All rights reserved.
- * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF Avaya Inc.
- * The copyright notice above does not evidence any actual
- * or intended publication of such source code.
- */
-
 
 public class ResumeApplication extends Application {
     private ApplicationComponent applicationComponent;
