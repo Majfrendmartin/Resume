bd9d56117fa0dbe3b2746fbd463c88b9822d4442
Majfrendmartin <raciborski.p@gmail.com>
2017-08-17 12:52:16 +0200

    Hidding splash on glide error

--- app/src/main/java/com/wec/resume/view/MainActivity.java
+++ app/src/main/java/com/wec/resume/view/MainActivity.java
@@ -111,6 +111,7 @@ public class MainActivity extends AbstractPresenterActivity<MainActivityPresente
                 .listener(new RequestListener<String, GlideDrawable>() {
                     @Override
                     public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        presenter.bioImageReady();
                         return false;
                     }
 
