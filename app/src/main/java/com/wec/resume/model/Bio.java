af34a0b6ee043f2d4fb7a7976435581c5e6b3ca4
Majfrendmartin <raciborski.p@gmail.com>
2017-08-27 11:49:07 +0200

    Proper description handling

--- app/src/main/java/com/wec/resume/model/Bio.java
+++ app/src/main/java/com/wec/resume/model/Bio.java
@@ -1,6 +1,8 @@
 package com.wec.resume.model;
 
 
+import android.support.annotation.Nullable;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -10,6 +12,9 @@ public class Bio {
     private String name;
     private String surname;
     private String avatar;
+
+    @Nullable
     private String description;
+
     private Social[] socials;
 }
